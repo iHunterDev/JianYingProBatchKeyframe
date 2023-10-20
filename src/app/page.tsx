40acaa8becb1f0fbe56d45fc9eea06b18210ea43
wzblog <wcgcodes@gmail.com>
2023-10-20 10:17:07 +0800

    feat: 添加页尾相关信息

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import { useRef } from "react";
 
 export default function Home() {
-
   const inputFileRef = useRef<HTMLInputElement>(null);
 
   async function handleUploadDraft() {
@@ -13,34 +12,32 @@ export default function Home() {
     const file = inputFileRef.current.files[0];
     try {
       const response = await fetch(`/api/generate?filename=${file.name}`, {
-        method: 'POST',
+        method: "POST",
         body: file,
       });
 
-      console.log("response", response)
-
+      console.log("response", response);
 
       if (!response.ok) {
         console.log("errMsg", await response.json());
-        throw new Error('处理失败，请检查文件是否正确');
+        throw new Error("处理失败，请检查文件是否正确");
       }
 
       // 前端下载文件
       const blob = await response.blob();
       const url = window.URL.createObjectURL(blob);
-  
+
       // 执行下载 for react
-      const link = document.createElement('a');
+      const link = document.createElement("a");
       link.href = url;
-      link.setAttribute('download', 'draft_info.json');
+      link.setAttribute("download", "draft_info.json");
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
     } catch (error) {
       console.log(error);
-      alert('处理失败，请检查文件是否正确');
+      alert("处理失败，请检查文件是否正确");
     }
-    
   }
 
   return (
@@ -111,7 +108,8 @@ export default function Home() {
                     选择你的剪映草稿文件
                   </p>
                   <p className="text-sm text-[#636262]">
-                    在你的操作系统中找到草稿文件夹，选择 `draft_info.json` 文件。
+                    在你的操作系统中找到草稿文件夹，选择 `draft_info.json`
+                    文件。
                   </p>
                 </div>
                 <div className="grid grid-cols-1 gap-4 rounded-xl bg-[#131313] p-8">
@@ -128,6 +126,100 @@ export default function Home() {
               </div>
             </div>
           </section>
+
+          <footer
+            className="bg-center text-white"
+          >
+            <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-32">
+              <div className="flex flex-col items-center">
+                {/* <a href="#" className="mb-8 inline-block max-w-full">
+                  <img
+                    src="https://assets.website-files.com/63904f663019b0d8edf8d57c/6399728d302d2471f18b229f_Group%2047874%20(2).svg"
+                    alt=""
+                    className="inline-block max-h-10"
+                  />
+                </a> */}
+                {/* <div className="text-center font-semibold max-[991px]:ml-0 max-[991px]:mr-0 max-[991px]:py-1">
+                  <a
+                    href="#"
+                    className="inline-block px-6 py-2 font-normal transition hover:text-[#d6a701]"
+                  >
+                    About
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-block px-6 py-2 font-normal transition hover:text-[#d6a701]"
+                  >
+                    Features
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-block px-6 py-2 font-normal transition hover:text-[#d6a701]"
+                  >
+                    Works
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-block px-6 py-2 font-normal transition hover:text-[#d6a701]"
+                  >
+                    Support
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-block px-6 py-2 font-normal transition hover:text-[#d6a701]"
+                  >
+                    Help
+                  </a>
+                </div> */}
+                <div className="mb-8 mt-8 w-48 border-b border-solid border-b-white"></div>
+                {/* <div className="mb-12 grid w-full max-w-[208px] grid-flow-col grid-cols-4 gap-3">
+                  <a
+                    href="#"
+                    className="ifont-bold mx-auto flex max-w-[24px] flex-col"
+                  >
+                    <img
+                      src="https://assets.website-files.com/63904f663019b0d8edf8d57c/639972bc5e36f4a882999413_Frame%205479.svg"
+                      alt=""
+                      className="inline-block"
+                    />
+                  </a>
+                  <a
+                    href="#"
+                    className="mx-auto flex max-w-[24px] flex-col font-bold"
+                  >
+                    <img
+                      src="https://assets.website-files.com/63904f663019b0d8edf8d57c/639972bf093252f2b2114050_Frame%205480.svg"
+                      alt=""
+                      className="inline-block"
+                    />
+                  </a>
+                  <a
+                    href="#"
+                    className="ifont-bold mx-auto flex max-w-[24px] flex-col"
+                  >
+                    <img
+                      src="https://assets.website-files.com/63904f663019b0d8edf8d57c/639972bde1a389ee15d86fc6_Frame%205481-1.svg"
+                      alt=""
+                      className="inline-block"
+                    />
+                  </a>
+                  <a
+                    href="#"
+                    className="mx-auto flex max-w-[24px] flex-col font-bold"
+                  >
+                    <img
+                      src="https://assets.website-files.com/63904f663019b0d8edf8d57c/639972bf10337117b26b8e51_Frame%205481.svg"
+                      alt=""
+                      className="inline-block"
+                    />
+                  </a>
+                </div> */}
+                <p className="max-[479px]:text-sm">
+                  Made with 😆 by <a href="https://twitter.com/HunterZhuo" target="_blank">HunterZhuo</a>. Powered by <a href="https://vercel.com/" target="_blank">Vercel</a>, <a href="https://www.cloudflare.com/" target="_blank">Cloudflare</a>.
+                </p>
+              </div>
+            </div>
+          </footer>
         </div>
       </div>
     </header>
