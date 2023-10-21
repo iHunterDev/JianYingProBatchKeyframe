cd3f85263e69100a5f48971b9b380bff3918ce4c
wzblog <wcgcodes@gmail.com>
2023-10-21 15:48:04 +0800

    fix: 修改因为 next/script 的 Script 组件使用不当导致无法编译

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,13 +1,13 @@
 import Script from "next/script";
 
-export default function () {
+export default function GoogleAnalytics() {
   return (
     <>
       {/* <!-- Google tag (gtag.js) --> */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-J46ZKC47K7"
       ></Script>
-      <Script>
+      <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
