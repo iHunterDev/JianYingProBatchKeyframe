08b8136e96ae3a53eefdb8c024bf22c4f15c651a
iHunterDev <wcgcodes@gmail.com>
2024-06-12 22:08:32 +0800

    feat(🆕): Add Copilot dashboard with draft processing

---
+++ src/app/[locale]/copilot/dashboard/dashboard.tsx
@@ -0,0 +1,124 @@
+"use client";
+import { useTranslations } from "next-intl";
+import { useEffect, useState, useRef } from "react";
+import Swal from 'sweetalert2'
+
+export default function DashboardComponent() {
+  const t = useTranslations("CopilotDashboard");
+
+  // 获取草稿列表
+  const [drafts, setDrafts] = useState([]);
+  useEffect(() => {
+    const fetchDrafts = async () => {
+      const response = await fetch("http://localhost:8080/api/v1/drafts");
+      const data = await response.json();
+      console.log("fetchDrafts", data);
+      setDrafts(data.data);
+    };
+
+    const timer = setTimeout(() => {
+      fetchDrafts();
+    }, 200);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  // 获取选中的草稿
+  const selecDraftRef = useRef<HTMLSelectElement>(null);
+  const [selectedDraft, setSelectedDraft] = useState<string>("");
+  async function handleSelectDraft() {
+    const draftJsonFile = selecDraftRef.current?.value;
+    // console.log("draftJsonFile", draftJsonFile);
+    if (!draftJsonFile) {
+      setSelectedDraft("");
+    } else {
+      setSelectedDraft(draftJsonFile);
+    }
+  }
+
+  // 一键处理草稿
+  async function handleProcessDraft(event) {
+    event.preventDefault();
+
+    // 获取选中的草稿内容
+    const selectedDraft = selecDraftRef.current?.value;
+    console.log("selectedDraft", selectedDraft);
+    if (!selectedDraft) {
+      Swal.fire({
+        title: t("UnselectedDraftTitle"),
+        icon: "warning"
+      });
+      return;
+    }
+
+    const response = await fetch(`http://localhost:8080/api/v1/draft?draft_json_file=${selectedDraft}`);
+    const result = await response.json();
+    // console.log("handleProcessDraft", result.data.draft_info);
+
+
+    // 处理草稿
+    const processedResponse = await fetch(`/api/generate?filename=draft_info.json`, {
+      method: "POST",
+      body: JSON.stringify(result.data.draft_info),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const processedDraft = await processedResponse.json();
+    // console.log("processedDraft", processedDraft);
+
+    // 保存处理后的草稿
+    const saveResponse = await fetch(`http://localhost:8080/api/v1/draft`, {
+      method: "POST",
+      body: JSON.stringify({
+        draft_json_file: selectedDraft,
+        draft_info: processedDraft
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const saveDraft = await saveResponse.json();
+    // console.log("saveDraft", saveDraft);
+
+    Swal.fire({
+      title: t("AddSuccessTitle"),
+      text: t("AddSuccessText"),
+      icon: "success"
+    });
+
+  }
+  return (
+    <div className="max-w-md mx-auto flex flex-col gap-y-10">
+      <div>
+        <label className="block mb-2 text-sm font-medium text-white">
+          {t("SelectDraft")}
+        </label>
+        <select
+          ref={selecDraftRef}
+          onChange={handleSelectDraft}
+          value={selectedDraft}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option selected value={""}>{t("SelectDraft")}</option>
+          {drafts.map((draft) => (
+            <option key={draft.draft_json_file} value={draft.draft_json_file}>
+              {draft.draft_name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <button
+        className="inline-block rounded-full bg-[#c9fd02] px-6 py-4 text-center font-bold text-black transition hover:border-black hover:bg-white"
+        onClick={handleProcessDraft}
+      >
+        {t("AddButton")}
+      </button>
+    </div>
+  );
+}
---
+++ src/app/[locale]/copilot/dashboard/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { useTranslations } from "next-intl";
+import { useEffect, useState } from "react";
+import ReactLoading from "react-loading";
+import Dashboard from "./dashboard";
+
+export default function CopilotDashboard() {
+  const t = useTranslations("CopilotDashboard");
+
+  // 设置 Copilot 默认的 API 地址
+  useEffect(() => {
+    if (!window.localStorage.getItem("copilot_api_url")) {
+      window.localStorage.setItem("copilot_api_url", "http://localhost:9507");
+    }
+  })
+  
+
+  // 检测是否开启 Copilot
+  // 如果没开启则提示去启动，否则则显示 Copilot Dashboard
+  // 如何检测呢？ 去请求本地的 localhost:8080 接口，如果能正常响应则说明 Copilot 已经启动，同时 localhost:8080 为默认值，host 地址从 localstorage 中读取
+
+  const [isCopilotEnabled, setIsCopilotEnabled] = useState(false);
+
+  useEffect(() => {
+    const checkCopilotStatus = async () => {
+      const response = await fetch("http://localhost:8080");
+      const data = await response.text();
+      console.log(data);
+
+      if (response.status === 200) {
+        setIsCopilotEnabled(true);
+      } else {
+        setIsCopilotEnabled(false);
+      }
+    };
+
+    const timer = setTimeout(() => {
+      checkCopilotStatus();
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <section className="relative">
+      {/* BG Image */}
+      <img
+        src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28f2_Background%20Image1%20(1).svg"
+        alt=""
+        className="absolute -z-10 inline-block h-full w-full object-cover"
+      />
+      {/* Container */}
+      <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-32">
+        {/* Component */}
+
+        {/* 检测到 Copilot 是否开启 */}
+        {/* 如果没开启则提示去启动，否则则显示 Copilot Dashboard */}
+        {isCopilotEnabled ? (
+          <Dashboard />
+        ) : (
+          <div className="flex flex-col items-center justify-center gap-y-10">
+            <ReactLoading type="bars" color="#ffffff" width={80} height={80} />
+            {/* <p className="text-center text-3xl font-bold text-white">{t("Copilot is not running")}</p> */}
+            <p className="text-center text-3xl font-bold text-white">
+              {t("CheckingCopilotTips")}
+            </p>
+            <p className="text-center text-sm text-white">
+              {t("CheckingCopilotHelpTips")} <a href="./" className="hover:text-[#c9fd02] underline">{t("DownloadClient")}</a>
+            </p>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+}
