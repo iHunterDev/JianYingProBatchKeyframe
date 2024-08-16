3d98eb47ce111cb4b51d8b30d8b63da49e75398b
iHunterDev <wcgcodes@gmail.com>
2024-08-16 20:31:43 +0800

    ✨ (changelog): add changelog page with support for multiple locales and dynamic content

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -63,6 +63,12 @@ export default function Navbar({ locale: currentLocale }: Props) {
                 {t("Donate")}
               </Link>
               <Link
+                className="font-inter rounded-lg hover:text-[#c9fd02] lg:px-6 lg:py-4"
+                href={"/"+ currentLocale + "/changelog"}
+              >
+                {t("Changelog")}
+              </Link>
+              <Link
                 className="font-inter rounded-lg hover:text-[#c9fd02] lg:px-6 lg:py-4 flex items-center gap-1"
                 href="https://github.com/iHunterDev/JianYingProBatchKeyframe"
                 target="_blank"
